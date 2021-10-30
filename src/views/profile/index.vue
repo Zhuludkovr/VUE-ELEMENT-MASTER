6483d2f9fe2a16f160b2248bdd91a38f2a13ccb5
Роман <rzhuludkov@bk.ru>
2021-10-30 18:27:04 +0300

    Profile

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -2,12 +2,11 @@
   <div class="app-container">
     <div v-if="user">
       <el-row :gutter="20">
-
-        <el-col :span="6" :xs="24">
+        <el-col :span="24" :xs="24">
           <user-card :user="user" />
         </el-col>
 
-        <el-col :span="18" :xs="24">
+        <!--<el-col :span="18" :xs="24">
           <el-card>
             <el-tabs v-model="activeTab">
               <el-tab-pane label="Activity" name="activity">
@@ -21,48 +20,43 @@
               </el-tab-pane>
             </el-tabs>
           </el-card>
-        </el-col>
-
+        </el-col>-->
       </el-row>
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import UserCard from './components/UserCard'
+import { mapGetters } from "vuex";
+import UserCard from "./components/UserCard";
 import Activity from './components/Activity'
 import Timeline from './components/Timeline'
 import Account from './components/Account'
 
 export default {
-  name: 'Profile',
+  name: "Profile",
   components: { UserCard, Activity, Timeline, Account },
   data() {
     return {
       user: {},
-      activeTab: 'activity'
-    }
+      activeTab: "activity",
+    };
   },
   computed: {
-    ...mapGetters([
-      'name',
-      'avatar',
-      'roles'
-    ])
+    ...mapGetters(["name", "avatar", "roles"]),
   },
   created() {
-    this.getUser()
+    this.getUser();
   },
   methods: {
     getUser() {
       this.user = {
         name: this.name,
-        role: this.roles.join(' | '),
-        email: 'admin@test.com',
-        avatar: this.avatar
-      }
-    }
-  }
-}
+        role: this.roles.join(" | "),
+        email: "admin@test.com",
+        avatar: this.avatar,
+      };
+    },
+  },
+};
 </script>
